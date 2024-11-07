e107ec20f6480375d06017a79c245ef329c0ba70
aarthoti <165478144+aarthoti@users.noreply.github.com>
2024-11-07 14:53:33 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,15 @@
+import java.lang.System;
+import java.lang.String;
+class Student{
+public static void main(String arg[])
+{
+int sno=1;
+String sname="suppi";
+String sclass="MCA";
+Float ssal=0F;
+System.out.println("THE GIVEN STUDENT NUMBER:"+sno);
+System.out.println("THE GIVEN STUDENT NAME :"+sname);
+System.out.println("THE GIVEN STUDENT CLASS :"+sclass);
+System.out.println("THE GIVEN STUDENT SLARY:"+ssal);
+}
+}
